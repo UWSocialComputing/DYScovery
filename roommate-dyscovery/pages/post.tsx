c236e7afdafe9b9e48674ae731f4a8a35418e512
Diana Dai <dianadai.dy@gmail.com>
2023-05-02 12:49:20 -0700

    replace the template withe some static data

---
+++ roommate-dyscovery/pages/post.tsx
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @returns A post component that has the posts. 
+ */
+
+export default function Post() {
+    return (
+      <ul>
+        This is an interesting post
+      </ul>
+    )
+  }
